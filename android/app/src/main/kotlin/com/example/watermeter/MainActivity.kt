8770e228fa0c1ea9501b9e7281689c4204115d56
satha jasi <151600912+sathajasioff@users.noreply.github.com>
2025-08-22 23:09:11 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/watermeter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.watermeter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/Login/logi.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({super.key});
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Text("login Screen"),
+      appBar: AppBar(
+        title: const Text("Login ScreeBBGHGHn"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+          // Action to perform when the button is pressed
+          print("Floating Action Button Pressed");
+        },
+        child: const Icon(Icons.dangerous_sharp),
+      ),  
+    );
+  }
+}
