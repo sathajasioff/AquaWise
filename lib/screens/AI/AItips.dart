a1e8e8564127c500d177ee041f36bc6bc07fea04
satha jasi <151600912+sathajasioff@users.noreply.github.com>
2025-10-10 13:01:11 +0530

    done

--- lib/screens/AI/AItips.dart
+++ lib/screens/AI/AItips.dart
@@ -1104,7 +1104,7 @@ Make the tips highly relevant, practical, and tailored to a $persona's typical w
               Text(
                 "Rainwater Harvesting Simulator",
                 style: GoogleFonts.poppins(
-                  fontSize: 16,
+                  fontSize: 12,
                   fontWeight: FontWeight.w600,
                   color: const Color(0xFF1A2B47),
                 ),
